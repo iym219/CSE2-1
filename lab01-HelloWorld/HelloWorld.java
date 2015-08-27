30c7031b2c384c0e7542b81598257aabdff1f3d1
hwd219 <hwd219@lehigh.edu>
2015-08-27 15:37:02 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,25 @@
+//Henry Daly
+//Lab 1
+//Hello World Java Program
+//
+
+//first one must create a public class
+public class HelloWorld{
+
+
+//then making a main method is the next course of action
+    public static void main(String[] args) {
+
+//finally, I have printed Hello World as well as my name with a box around it
+        System.out.println("###################");
+        System.out.println("###################");
+        System.out.println("# Hello World     #");
+        System.out.println("# My Name Is Henry#");
+        System.out.println("###################");
+        System.out.println("###################");
+
+//this wraps up the end of my first program
+
+ }
+    
+}
