c9ea2f706bfed5676aa4bcccf0c61ed0ef9cacb2
hwd219 <hwd219@lehigh.edu>
2015-08-31 00:17:43 +0000

    This is all of homework 1 assignment

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,26 @@
+//Henry Daly
+//CSE 002
+//HW 01
+//Aug 28
+
+
+public class WelcomeClass {
+//Making a class so that the code will work
+    
+    public static void main(String[] args) {
+        //this is the main method right here
+        
+        System.out.println("-----------------");
+        System.out.println("|    WELCOME    |");
+        System.out.println("-----------------");
+        System.out.println("  ^  ^  ^  ^  ^  ^  ");
+        System.out.println("|-H--W--D--2--1--9-|");
+        System.out.println("  v  v  v  v  v  v  ");
+        System.out.println("This June I travelled to Colorado, and I decided to take a nap at 13000ft");
+        System.out.println("while hiking a mountain. A mountain goat woke me up");
+        System.out.println("and was standing 3 ft away from me.");
+        //this will print out an amazing welcome message and tell a fantastic story
+        
+    }
+    
+}
